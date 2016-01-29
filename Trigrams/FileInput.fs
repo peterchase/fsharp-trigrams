969af05b3fdbad6b9e6b1ac720bf9a92dc5e1914
Peter Chase <peter.chase@grantadesign.com>
2016-01-29 14:17:57 +0000

    Kinda read the file

---
+++ Trigrams/FileInput.fs
@@ -0,0 +1,10 @@
+﻿namespace Trigrams
+
+module FileInput =
+
+    let getElements fileName =
+        seq { use sr = System.IO.File.OpenText(fileName)
+              while not sr.EndOfStream do
+                 let line = sr.ReadLine() 
+                 let words = line.Split ' '
+                 yield! words }
--- Trigrams/Program.fs
+++ Trigrams/Program.fs
@@ -1,7 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open Trigrams
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    FileInput.getElements argv.[0] |> Seq.iter (string >> printfn "%s")
     0 // return an integer exit code
