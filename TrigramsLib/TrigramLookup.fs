f4bd7535cb9b969a4ada508707dc94faf5ad1c6a
Peter Chase <peter.chase@grantadesign.com>
2016-01-29 15:30:25 +0000

    Attempt grouping the trigrams by their first pair

---
+++ TrigramsLib/TrigramLookup.fs
@@ -0,0 +1,9 @@
+﻿namespace Trigrams
+
+module TrigramLookup =
+
+    let private third (_, _, c) = c
+
+    let toTwoWordLookup trigramList =
+        trigramList |> List.groupBy (fun (x, y, _) -> (x, y))
+    
